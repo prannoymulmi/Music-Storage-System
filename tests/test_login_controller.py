6e8d34d4d9238120fc93f92c29cd71080aa28137
prannoy <prannoy.mulmi@gmail.com>
2023-07-04 20:55:43 +0200

    Fixed login test

--- tests/test_login_controller.py
+++ tests/test_login_controller.py
@@ -1,8 +1,18 @@
+from unittest import mock
+from unittest.mock import MagicMock
+
+from sqlmodel import Session
+
+from models.user import User
+from repositories import user_repository
 from src.controllers.login_controller import LoginController
 
 
-def test_login():
+@mock.patch.object(user_repository.UserRepository, "get_user_id")
+def test_login(mock_user_repo):
+    mock_user_repo.return_value = User(password="password")
+    mock_session = MagicMock(Session)
     login = LoginController()
-    result = login.login("some_user", "password")
+    result = login.login("some_user", "password", mock_session)
     assert result == "logged in"
 
